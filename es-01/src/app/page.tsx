4dd1bd02836e286b917782207320a68983e28bca
Bjarke Baltzersen <Bbaltzersen@hotmail.com>
2025-01-25 01:14:14 +0100

    Auth works with next15

--- es-01/src/app/page.tsx
+++ es-01/src/app/page.tsx
@@ -1,9 +1,20 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { auth0 } from "@/lib/auth0"
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth0.getSession()
+
+  if (!session) {
+    return (
+      <main>
+        <a href="/auth/login?screen_hint=signup">Sign up</a>
+        <a href="/auth/login">Log in</a>
+      </main>
+    )
+  }
+
   return (
-    <>
-    </>
-  );
+    <main>
+      <h1>Welcome, {session.user.name}!</h1>
+    </main>
+  )
 }
