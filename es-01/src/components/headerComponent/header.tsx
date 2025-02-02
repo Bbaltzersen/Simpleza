fb727a2020b859e075969b72e7aec2004c9fcb53
Bjarke Baltzersen <Bbaltzersen@hotmail.com>
2025-02-02 11:54:50 +0100

    I think this fix the log in

--- es-01/src/components/headerComponent/header.tsx
+++ es-01/src/components/headerComponent/header.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React from "react";
 import Logo from "./headerLogo/logo";
 import Menu from "./headerMenu/menu";
--- es-01/src/components/headerComponent/headerMenu/menu.tsx
+++ es-01/src/components/headerComponent/headerMenu/menu.tsx
@@ -1,12 +1,13 @@
-"use client"
-
-import React, { useState } from "react";
 import styles from './menu.module.css'
 import NavMenuButton from './navMenuButton/navMenuButton';
 import Register from "./regMenuButton/register";
 import SignIn from "./sigMenuButton/signIn";
+import { auth0 } from '@/lib/auth0';
 
-const Menu = () => {
+const Menu = async () => {
+    const session = await auth0.getSession();
+
+    if(!session){
     return (
         <div className={styles.container}>
             <SignIn/>
@@ -14,6 +15,13 @@ const Menu = () => {
             <NavMenuButton/>
         </div>
     );
+    }
+
+    return(
+        <div className={styles.container}>
+            <NavMenuButton/>
+        </div>
+    )
 };
 
 export default Menu;
