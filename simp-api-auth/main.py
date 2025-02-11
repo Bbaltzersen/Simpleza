bc78aff9a3ec569b1e633d92da92d2bcbc8f0cc3
Bjarke Baltzersen <Bbaltzersen@hotmail.com>
2025-02-11 21:01:56 +0100

    Update main.py

--- simp-api-auth/main.py
+++ simp-api-auth/main.py
@@ -1,9 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from api.authentication_routes import router as auth_router
 from api.authorization_routes import router as authz_router
 
 app = FastAPI()
 
-# Include Authentication and Authorization Routes
-app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
-app.include_router(authz_router, prefix="/authz", tags=["Authorization"])
+# Define API versioning
+api_v1 = APIRouter(prefix="/v1")
+
+# Include Authentication and Authorization Routes under `/v1`
+api_v1.include_router(auth_router, prefix="/authentication", tags=["Authentication"])
+api_v1.include_router(authz_router, prefix="/authorization", tags=["Authorization"])
+
+# Register `/v1` with FastAPI app
+app.include_router(api_v1)
