e97e078a5cf22f04f663e4387c23956461c788cc
Bjarke Baltzersen <Bbaltzersen@hotmail.com>
2025-02-24 15:46:23 +0100

    signout works now

--- simp-front-end/src/components/headerComponent/headerMenu/signOutMenuButton/signOut.tsx
+++ simp-front-end/src/components/headerComponent/headerMenu/signOutMenuButton/signOut.tsx
@@ -1,11 +1,23 @@
-import styles from "./signOut";
+"use client";
 
-import React from 'react'
+import React from 'react';
+import { useAuth } from '@/lib/context/authContext';
 
-function SignOut() {
+const SignOut: React.FC = () => {
+  const { logout } = useAuth();
+
+  const handleSignOut = async (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    await logout();
+    // Optionally, redirect or reload the page after logging out
+    window.location.reload();
+  };
+
   return (
-    <a href="/auth/logout">Sign out</a>
-  )
-}
+    <a href="#" onClick={handleSignOut}>
+      Sign out
+    </a>
+  );
+};
 
-export default SignOut
+export default SignOut;
