1df23e2cfaee21f1530823c9ede6c8a028f25901
Bjarke Baltzersen <Bbaltzersen@hotmail.com>
2025-03-09 00:03:54 +0100

    added models

--- simp-api-recipes/models/__init__.py
+++ simp-api-recipes/models/__init__.py
@@ -1,6 +1,23 @@
 from .base import Base
 from .user import User
+from .recipe import Recipe
+from .recipe_author import RecipeAuthor
+from .recipe_ingredient import RecipeIngredient
+from .recipe_tag import RecipeTag
+from .recipe_image import RecipeImage
+from .recipe_step import RecipeStep
+from .recipe_favorite import RecipeFavorite
+from .tag import Tag
+
 __all__ = [
     "Base",
     "User",
+    "Recipe",
+    "RecipeAuthor",
+    "RecipeIngredient",
+    "RecipeTag",
+    "RecipeImage",
+    "RecipeStep",
+    "RecipeFavorite",
+    "Tag",
 ]
