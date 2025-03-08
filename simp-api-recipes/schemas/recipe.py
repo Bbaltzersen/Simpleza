d7626c16dd51cb3200b4e67aaa29ed9a685569e2
Bjarke Baltzersen <Bbaltzersen@hotmail.com>
2025-03-09 00:33:30 +0100

    schemas added

---
+++ simp-api-recipes/schemas/recipe.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel
+from typing import List, Optional
+from uuid import UUID
+
+class RecipeOut(BaseModel):
+    recipe_id: uuid.UUID
+    title: str
+
+    class Config:
+        from_attributes = True
+
+
+class RecipeIngredientSchema(BaseModel):
+    ingredient_id: UUID
+    amount: float
+    measurement: str
+
+class RecipeStepSchema(BaseModel):
+    step_number: int
+    description: str
+    image_url: Optional[str] = None
+
+class RecipeImageSchema(BaseModel):
+    image_url: str
+
+class RecipeCreateSchema(BaseModel):
+    title: str
+    description: Optional[str] = None
+    ingredients: List[RecipeIngredientSchema]
+    steps: List[RecipeStepSchema]
+    images: List[RecipeImageSchema]
+    tags: List[UUID]  # Using tag IDs (Alternatively, use List[str] for tag names)
+
+    class Config:
+        orm_mode = True
