1ccd63d89d6fbbf2941d619f4bef2666529d3390
Bjarke Baltzersen <Bbaltzersen@hotmail.com>
2025-03-08 23:52:46 +0100

    now running as expected

--- simp-api-recipes/main.py
+++ simp-api-recipes/main.py
@@ -21,4 +21,4 @@ app.add_middleware(
 )
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8010, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8020, reload=True)
