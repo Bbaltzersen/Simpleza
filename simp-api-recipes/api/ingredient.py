6d36b34a4577185e7a592e5f477fdf8c0df95650
Bjarke Baltzersen <Bbaltzersen@hotmail.com>
2025-03-14 04:34:17 +0100

    last working version

--- simp-api-recipes/api/ingredient.py
+++ simp-api-recipes/api/ingredient.py
@@ -1,5 +1,6 @@
 from typing import List
 from fastapi import APIRouter, Depends, HTTPException, Query
+from sqlalchemy import func
 from sqlalchemy.orm import Session
 import uuid
 from database.connection import SessionLocal
@@ -63,7 +64,16 @@ def delete_ingredient(ingredient_id: str, db: Session = Depends(get_db)):
     return {"message": "Ingredient deleted successfully"}
 
 @router.get("/", response_model=List[IngredientSchema])
-def search_ingredients(search: str = Query(..., min_length=3), db: Session = Depends(get_db)):
-    search_term = f"%{search}%"
-    ingredients = db.query(Ingredient).filter(Ingredient.name.ilike(search_term)).limit(10).all()
+def search_ingredients(
+    search: str = Query(..., min_length=3),
+    db: Session = Depends(get_db)
+):
+    # Use similarity() to filter ingredients that are at least 40% similar
+    ingredients = (
+        db.query(Ingredient)
+          .filter(func.similarity(Ingredient.name, search) >= 0.8)
+          .order_by(func.similarity(Ingredient.name, search).desc())
+          .limit(10)
+          .all()
+    )
     return ingredients
